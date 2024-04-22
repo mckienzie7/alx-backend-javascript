cfb81924049ed9735445b462ab55f1b2cf8c16a5
Michael Solomon <Michaelelsa12@gmail.com>
2024-04-23 01:31:42 +0300

    configration file

---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Transpile modern JavaScript to a compatible version
+    '@babel/preset-react' // Transpile JSX syntax for React
+  ],
+  plugins: [
+    // Additional Babel plugins can be added here based on your needs
+  ]
+};
+
