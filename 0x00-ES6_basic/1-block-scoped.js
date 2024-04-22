b1c2dd3f27e180cdfe981c161eb892d1e4e9499f
Michael Solomon <michaelelsa12@gmail.com>
2024-04-23 01:57:32 +0300

    const js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // eslint-disable-line no-unused-vars
+  const task2 = true; // eslint-disable-line no-unused-vars
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
