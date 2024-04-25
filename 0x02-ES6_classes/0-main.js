66e371f3c91c66a9371e9a6fcd1fb30fa8b47225
Michael Solomon <michaelelsa12@gmail.com>
2024-04-26 00:48:49 +0300

    1st classes

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,4 +1,4 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
-const rrom = new ClassRoom(10);
-console.log(room._maxStudentSize)
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize);
